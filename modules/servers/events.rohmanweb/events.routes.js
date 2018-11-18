8f9337f1f0b2041fc85a6206e0e43af60dbe8546
rohmanmail@gmail.com <rohmanahmad>
2018-11-18 17:39:27 +0700

    update

---
+++ modules/servers/events.rohmanweb/events.routes.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const utils = use('Utils.Helper')
+const func = require('./events.func')
+const allRoutes = use('All.Routes')
+const path = use('path')
+const _ = use('_')
+
+const listRoutes = {
+    post: [
+    ],
+    get: [
+        {
+            path: '/',
+            fn: func.main
+        },
+        {
+            path: '/*',
+            fn: function (req, res) {
+                res.send({
+                    status: 404,
+                    message: 'NOT FOUND'
+                })
+            }
+        }
+    ]
+}
+
+function getMiddlewares (r, prefix) {
+    const middlewares = r.middlewares || []
+    return {
+        route: path.join(prefix, r.path),
+        funcs: middlewares,
+        handler: r.fn
+    }
+}
+
+const register = function (app, prefix = '/events') {
+    // register another routes
+    // allRoutes.register(app, func, prefix)
+    let routes = []
+    for (let r of listRoutes['post']) {
+        const {route, funcs, handler} = getMiddlewares(r, prefix)
+        const slash = route.replace(/[^/]/g, '')
+        utils.debugme(` |-- registering route: ${route} [POST]`)
+        routes.push({type: 'post', route, funcs, handler, total: slash.length})
+        // app.post(route, funcs, handler)
+    }
+    for (let r of listRoutes['get']) {
+        const {route, funcs, handler} = getMiddlewares(r, prefix)
+        const slash = route.replace(/[^/]/g, '')
+        utils.debugme(` |-- registering route: ${route} [GET]`)
+        routes.push({type: 'get', route, funcs, handler, total: slash.length})
+        // app.get(route, funcs, handler)
+    }
+    routes = _.orderBy(routes, ['total'], ['desc'])
+    for (const r of routes) {
+        if (r.type === 'get') {
+            app.get(r.route, r.funcs, r.handler)
+        } else if (r.type === 'post') {
+            app.post(r.route, r.funcs, r.handler)
+        }
+    }
+}
+
+module.exports = {
+    register
+}
